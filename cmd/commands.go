06edb1b8a668800dd779bd89e9b43030a91692cd
ultramozg <d.candu@hotmail.com>
2020-06-01 22:19:26 +0300

    update commands

--- cmd/commands.go
+++ cmd/commands.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 	"os"
 
@@ -11,8 +12,17 @@ var rootCmd = &cobra.Command{
 	Use:   "download",
 	Short: "Download file",
 	Long:  "gget utility is using for download and upload files via http",
+	Args: func(cmd *cobra.Command, args []string) error {
+		if len(args) < 2 {
+			return errors.New("requires a URL")
+		}
+		/*if myapp.IsValidColor(args[0]) {
+			return nil
+		}*/
+		return fmt.Errorf("invalid URL specified: %s", args[1])
+	},
 	Run: func(cmd *cobra.Command, args []string) {
-		// Do Stuff Here
+		fmt.Println(args)
 	},
 }
 
